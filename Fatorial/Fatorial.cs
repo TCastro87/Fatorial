bf7c6619af79318b0a602b4b22740757b8b37f5e
Tiago Castro <31635449+TCastro87@users.noreply.github.com>
2018-04-04 19:42:25 -0300

    Liberty

---
+++ Fatorial/Fatorial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fatorial
+{
+    class Fatorial
+    {
+        //Fatorial de N com resultado.
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Qual numero deseja saber o fatorial? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+            Console.ReadLine();
+        }
+    }
+}
